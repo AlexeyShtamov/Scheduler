94b4d0f58f77c45a18bd2cb4d4b134a54887c45d
Waltans <suntsovvl00@yandex.ru>
2024-11-06 20:25:24 +0500

    123

--- src/main/java/ru/develop/schedule/domain/Task.java
+++ src/main/java/ru/develop/schedule/domain/Task.java
@@ -23,7 +23,7 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     /**
-     * dasdas
+     * dasdsadsa
      */
     private String title;
     private String description;
